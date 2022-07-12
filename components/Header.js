a5fda3960ab1f25004d58e669b45c8449cab71e0
Andrew Wichmann <awichmann@mintel.com>
2022-07-12 14:35:54 -0400

    better header

--- components/Header.js
+++ components/Header.js
@@ -35,7 +35,6 @@ export default function Header({ title }) {
           Home
         </Link>
         <Link href="/about">About</Link>
-        {loggedIn ? <a href="/profile">My Pledge</a> : <></>}
         <a
           id={styles.oneOffDonation}
           href="https://interland3.donorperfect.net/weblink/weblink.aspx?name=E190660&id=6"
@@ -46,12 +45,12 @@ export default function Header({ title }) {
       <div className={styles.headerRight}>
         {loggedIn ? (
           <>
-            <div>{user && <>Welcome {user?.user_metadata.full_name}!</>}</div>{" "}
-            <br />
-            <button onClick={logout}>Log out here.</button>
+            <div>{user && <>Welcome {user?.user_metadata.full_name}!</>}</div>
+            <a href="/profile">My pledge</a>
+            <a onClick={logout}>Log out</a>
           </>
         ) : (
-          <button onClick={login}>Log in here.</button>
+          <a onClick={login}>Log in</a>
         )}
       </div>
     </div>
