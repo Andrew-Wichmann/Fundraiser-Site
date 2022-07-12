5d4a4dc2a459048a5672a4d72fd9d0812b1716e4
Andrew Wichmann <awichmann@mintel.com>
2022-07-12 00:41:12 -0400

    add basic auth

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,32 @@
+import { useState, useEffect } from "react";
+import netlifyAuth from "../lib/netlifyAuth";
 import Link from "next/link";
 
 import styles from "./Header.module.css";
 
 export default function Header({ title }) {
+  let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated);
+  let [user, setUser] = useState(null);
+  let login = () => {
+    netlifyAuth.authenticate((user) => {
+      setLoggedIn(!!user);
+      setUser(user);
+      netlifyAuth.closeModal();
+    });
+  };
+
+  let logout = () => {
+    netlifyAuth.signout(() => {
+      setLoggedIn(false);
+      setUser(null);
+    });
+  };
+
+  useEffect(() => {
+    netlifyAuth.initialize((user) => {
+      setLoggedIn(!!user);
+    });
+  }, [loggedIn]);
   return (
     <div className={styles.header}>
       <div className={styles.headerLeft}>
@@ -19,7 +43,15 @@ export default function Header({ title }) {
         </a>
       </div>
       <div className={styles.headerRight}>
-        <a href="#login">Log in / Sign up (coming soon™)</a>
+        {loggedIn ? (
+          <>
+            <div>{user && <>Welcome {user?.user_metadata.full_name}!</>}</div>{" "}
+            <br />
+            <button onClick={logout}>Log out here.</button>
+          </>
+        ) : (
+          <button onClick={login}>Log in here.</button>
+        )}
       </div>
     </div>
   );
