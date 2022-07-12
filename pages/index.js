2ac52b4c2e3a8d392ef49c85d55dbaaa94050dee
Andrew Wichmann <awichmann@mintel.com>
2022-07-11 23:06:29 -0400

    more hoopla

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,18 @@ export default function Home() {
       </p>
       <p className="description">
         He needs to raise $2,000 and would like to earn that money rather than
-        simply asking for it.
+        simply asking for it, so he came up with a little accountability game.
       </p>
       <p className="description">
-        By signing up as a donor, you can pledge a donation for each mile biked,
-        every lap swum, and every step taken during his training.
+        By signing up to "play" as a donor, you can pledge a donation for each
+        mile biked, every lap swum, and every step taken during his training.
+      </p>
+      <p className="description">
+        On race day, the pledges will be tallied up sent to your email.
+      </p>
+      <p className="description">
+        Or, if that sounds like too much trouble, you can just simply make a
+        "one off donation" up above.
       </p>
     </div>
   );
